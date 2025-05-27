300f856fce4018796fd191a5604787c757bbf67e
Yuki Kurosawa Hotkey <yuki@ksyuki.com>
2025-05-27 13:37:50 +0800

    update deps

--- Application/YukiLoader/LVGLInit.c
+++ Application/YukiLoader/LVGLInit.c
@@ -124,7 +124,7 @@ void Draw()
     lv_obj_t * img = lv_image_create(lv_screen_active());
     /* Assuming a File system is attached to letter 'A'
      * E.g. set LV_USE_FS_STDIO 'A' in lv_conf.h */
-    lv_image_set_src(img, "B:/front.png");
+    lv_image_set_src(img, "A:/front.png");
     lv_obj_center(img);
 
 }
